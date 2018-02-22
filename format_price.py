827b5d9915ea427c59333f3f4b8e4e3703451f58
Ivan Yandunen <34913063+ivanyandunen@users.noreply.github.com>
2018-02-22 11:56:50 +0300

    useless float removed

--- format_price.py
+++ format_price.py
@@ -4,9 +4,9 @@ import argparse
 def format_price(price):
     try:
         if price - int(price) == 0:
-            return '{:,.0f}'.format(float(price)).replace(',', ' ')
+            return '{:,.0f}'.format(price).replace(',', ' ')
         else:
-            return '{:,.2f}'.format(float(price)).replace(',', ' ')
+            return '{:,.2f}'.format(price).replace(',', ' ')
     except (ValueError, SyntaxError, TypeError):
         return None
 
